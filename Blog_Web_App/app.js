d4243c979ad926525dbc684f4433e7059a6f0265
raviacts035 <ravindrainturi035@gmail.com>
2023-05-20 18:17:29 +0530

    Update : CSS and Functionality, ok

--- Blog_Web_App/app.js
+++ Blog_Web_App/app.js
@@ -5,23 +5,28 @@ useFetch=async ()=>{
 }
 
 const postContainer=document.getElementById("postContainer");
+const userPostContainer=document.getElementById("userPostContainer")
 const addPost=document.querySelector("button");
 const creatorBox=document.getElementById("creatorBox")
-const title=document.getElementById("title")
-const bodyX=document.getElementById("body")
+const titlex=document.getElementById("titlex")
+const bodyX=document.getElementById("bodyX")
+const mainx=document.querySelector("main");
 
 useFetch().then(arr=>{
     arr.forEach(post => {
         let ele=document.createElement('div');
-        ele.classList="border-2 p-4 rounded-xl";
+        ele.classList="flex justify-between group items-center border-2 hover:shadow-xl shadow-lg p-2 rounded-xl";
         ele.innerHTML=`
-        <p class="text-3xl">${post.title} </p>
+        <div class="">
+        <p class="text-2xl">${post.title} </p>
         <p>${post.body}</p>
-        <button class="" onclick='this.parentElement.remove()'>del</button>
+        </div>
+        <button class="bg-red-400 h-6 collapse group-hover:visible text-white px-2 bottom-20" onclick='this.parentElement.remove()'>x</button>
         `;
         ele.id=post.id
 
         postContainer.appendChild(ele)
+
     });
 });
 
@@ -29,20 +34,31 @@ addPost.addEventListener("click",handleAddPost);
 
 
 function handleAddPost(){
-    creatorBox.toggleAttribute("hidden")
+    creatorBox.classList.toggle('hidden')
+    mainx.classList.toggle('hidden')
+    titlex.focus()
+}
+
+function fun(){
+
 }
 
 function createNewPost(){
-    console.log("SuccesFull..")
-    let titleText=title.value;
+    let titleText=titlex.value;
     let bodyText=bodyX.value
 
     let ele=document.createElement('div');
-        ele.classList="border-2 p-4 rounded-xl";
+        ele.classList="flex justify-between group items-center border-2 hover:shadow-xl shadow-lg p-2 rounded-xl";
         ele.innerHTML=`
-        <p class="text-3xl">${titleText} </p>
+        <div class="">
+        <p class="text-2xl">${titleText} </p>
         <p>${bodyText}</p>
-        <button class="" onclick='this.parentElement.remove()'>del</button>
+        </div>
+        <button class="bg-red-400 h-6 collapse group-hover:visible text-white px-2 bottom-20" onclick='this.parentElement.remove()'>x</button>
         `;
-        postContainer.appendChild(ele)
-}
+        userPostContainer.appendChild(ele)
+        mainx.classList.toggle('hidden')
+        titlex.value=""
+        bodyText.value=""
+        creatorBox.classList.toggle('hidden')
+    }
